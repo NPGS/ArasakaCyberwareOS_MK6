4131ab2560113c9809f488b1d335a1dd031c9505
Nuccio Patrik <nucciopatriksch@gmail.com>
2022-10-20 01:03:10 +0200

    Starting editing for version 1.10.0

--- init.lua
+++ init.lua
@@ -1,25 +1,21 @@
--- ver. 1.9.2 - LUA MODULES LOADER
+-- ver. 1.10.0 - LUA MODULES LOADER
 
 registerForEvent("onInit", function()
-
-    -- List all modules to load
-
-        -- Cyberware
-        require("modules/berserk_fixes.lua")
-        require("modules/berserk_c5mk6.lua")
-        require("modules/cyberdecks_abilities.lua")
-        require("modules/cyberdeck_c4mk3.lua")    -- MILITECH GUEST
-        require("modules/cyberdeck_c4mk6.lua")    -- MILITECH GUEST
-        require("modules/cyberdeck_c5mk6.lua")
-        require("modules/hyperbandwidthlink_leg.lua")
-        require("modules/sandevistan_c4mk6.lua")    -- MILITECH GUEST
-        require("modules/sandevistan_c5mk3.lua")
-        require("modules/sandevistan_c5mk4.lua")
-        require("modules/sandevistan_c5mk6.lua")
-        require("modules/sandevistan_c6mk3.lua")  -- FUYUTSUKI GUEST
-        require("modules/trascendence_leg.lua")
-        require("modules/recipes.lua")
-
+    require("modules/berserk_fixes.lua")
+    require("modules/berserk_c5mk6.lua")
+    require("modules/cyberdecks_abilities.lua")
+    require("modules/cyberdeck_c4mk3.lua")    -- MILITECH GUEST
+    require("modules/cyberdeck_c4mk6.lua")    -- MILITECH GUEST
+    require("modules/cyberdeck_c5mk6.lua")
+    require("modules/cyberdeck_ttmk6.lua")  -- TETRATRONIC GUEST
+    require("modules/hyperbandwidthlink_leg.lua")
+    require("modules/sandevistan_c4mk6.lua")    -- MILITECH GUEST
+    require("modules/sandevistan_c5mk3.lua")
+    require("modules/sandevistan_c5mk4.lua")
+    require("modules/sandevistan_c5mk6.lua")
+    require("modules/sandevistan_c6mk3.lua")  -- FUYUTSUKI GUEST
+    require("modules/trascendence_leg.lua")
+    require("modules/recipes.lua")
 end)
 
 function arrayInsert(arr, rec)
