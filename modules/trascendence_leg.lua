d56caa48bc3f1bba12eb4a8f91334f5d1180e04c
Nuccio Patrik <nucciopatriksch@gmail.com>
2022-11-27 11:36:04 +0100

    Changed the effect of Trascendence: now it decreases the Sandevistan duration instead of increasing its cooldown.

--- modules/trascendence_leg.lua
+++ modules/trascendence_leg.lua
@@ -1,4 +1,4 @@
--- ver. 1.12.0
+-- ver. 1.12.2
 
 -- Sandevistan - Arasaka Trascendence (Legendary)
 if TweakDB:GetRecord("Items.SpecialSandevistanFragment2") == nil then
@@ -6,7 +6,7 @@ if TweakDB:GetRecord("Items.SpecialSandevistanFragment2") == nil then
         TweakDB:CloneRecord("Items.SpecialSandevistanFragment2_inline0", "Items.SandevistanFragment1_inline0")  -- Logic
             TweakDB:CloneRecord("Items.SpecialSandevistanFragment2_inline1", "Items.SandevistanFragment1_inline1")  -- UIData
             TweakDB:CloneRecord("Items.SpecialSandevistanFragment2_inline2", "Items.SandevistanC4MK5_inline2")  -- Time Dilation
-            TweakDB:CloneRecord("Items.SpecialSandevistanFragment2_inline3", "Items.SandevistanFragment4_inline2")  -- Inverse Cooldown
+            TweakDB:CloneRecord("Items.SpecialSandevistanFragment2_inline3", "Items.SandevistanFragment1_inline2")  -- Reduced Duration
     TweakDB:CloneRecord("Vendors.cct_dtn_ripdoc_01_trascendence", "Vendors.cct_dtn_ripdoc_01_inline7")
     TweakDB:SetFlat("Vendors.cct_dtn_ripdoc_01_trascendence.item", "Items.SpecialSandevistanFragment2")
     arrayInsert("Vendors.cct_dtn_ripdoc_01.itemStock", "Vendors.cct_dtn_ripdoc_01_trascendence")
@@ -20,9 +20,9 @@ TweakDB:SetFlat("Items.SpecialSandevistanFragment2.OnAttach", {"Items.SpecialSan
 TweakDB:SetFlat("Items.SpecialSandevistanFragment2.quality", "Quality.Legendary")
 TweakDB:SetFlat("Items.SpecialSandevistanFragment2.statModifiers", {"Items.SpecialSandevistanFragment2_inline2","Items.SpecialSandevistanFragment2_inline3"})
     TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline0.UIData", "Items.SpecialSandevistanFragment2_inline1")
-        TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline1.floatValues", {25.0, 4.0})
-        TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline1.localizedDescription", GetLocalizedTextByKey("Trascendence-UIDesc-Alt"))
+        TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline1.floatValues", {25.0, 25.0})
+        TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline1.localizedDescription", GetLocalizedTextByKey("Trascendence-UIDesc-Alt-2"))
     TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline2.modifierType", 'Multiplier')
     TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline2.value", 0.75)    -- Time Dilation multiplier %
-    TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline3.modifierType", 'Additive')
-    TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline3.value", -4.0)    -- Inverse Cooldown
+    TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline3.modifierType", 'Multiplier')
+    TweakDB:SetFlat("Items.SpecialSandevistanFragment2_inline3.value", 0.75)    -- Reduced Duration
