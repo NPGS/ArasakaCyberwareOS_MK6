b86fd82b9e1a85b888d06fe4e7cf9424785f1576
Nuccio Patrik <nucciopatriksch@gmail.com>
2023-01-28 16:25:42 +0100

    Added new hybrid cyberdeck "Kang Tao 'Acid' Mk.6" (temporary name).

--- modules/nightmare_mk45.lua
+++ modules/nightmare_mk45.lua
@@ -49,6 +49,23 @@ if TweakDB:GetRecord("Items.NetwatchQiantRageMK5_M") == nil then
     Ti200.arrayInsert("Debug.CW_Cyberdecks.items", "Items.NetwatchQiantRageMK5_M") -- Debug
 end
 
+-- Kang Tao Acid Mk.6
+if TweakDB:GetRecord("Items.KangTaoAcidMK6_M") == nil then
+    TweakDB:CloneRecord("Items.KangTaoAcidMK6_M", "Items.SandevistanC1MK1")    -- Item
+        TweakDB:CreateRecord("Items.KangTaoAcidMK6_M_inline0", "gamedataGameplayLogicPackage_Record")   -- Logic (Netrunner)
+            Ti200.cloneConstantStat("Items.KangTaoAcidMK6_M_inline1", "Items.MilitechParaline_inline1", 12)   -- RAM
+            Ti200.cloneConstantStat("Items.KangTaoAcidMK6_M_inline2", "Items.MilitechParaline_inline2", 10)  -- Buffer
+            Ti200.cloneConstantStat("Items.KangTaoAcidMK6_M_inline3", "Items.SandevistanC1MK1_inline1", 7)  -- Duration
+            Ti200.cloneConstantStat("Items.KangTaoAcidMK6_M_inline4", "Items.SandevistanC1MK1_inline2", 0.15)  -- Time Scale
+            Ti200.cloneConstantStat("Items.KangTaoAcidMK6_M_inline5", "Items.SandevistanC1MK1_inline3", 12)  -- Cooldown
+        Ti200.cloneConstantStat("Price.Cyberdeck_Hybrid_MK6", "Price.Cyberdeck", 20)   -- Price
+        TweakDB:CloneRecord("Items.KangTaoAcidMK6_M_inline6", "Items.SandevistanC1MK1_inline0")  -- Prereq
+    Ti200.arrayInsert("Items.KangTaoAcidMK6_M.tags", CName('Cyberdeck')) -- Tags
+    -- SERVICE
+    Ti200.createVendorItem("Vendors.std_arr_ripperdoc_01_acid_mk6","Vendors.DowntownCredAvailability","Items.KangTaoAcidMK6_M","Vendors.std_arr_ripperdoc_01.itemStock")-- Vendor
+    Ti200.arrayInsert("Debug.CW_Cyberdecks.items", "Items.KangTaoAcidMK6_M") -- Debug
+end
+
 local objActions = {
     "CyberwareAction.UseSandevistan",
     "DeviceAction.MalfunctionClassHack",
@@ -98,6 +115,19 @@ local objEquipRageMk5 = {
     "Items.NPCUploadTimeAbility02"
 }
 
+local objEquipHybridMk6 = {
+    "Items.SandevistanBase_inline2",
+    "Items.BaseDeck_inline0",
+    "Items.BaseDeck_inline3",
+    "Items.KangTaoAcidMK6_M_inline0",
+    "Items.UltimateHacksSpreadAbility",
+    "Items.DamageHacksAreaAbility",
+    "Items.CooldownReductionAbility03",
+    "Items.SpreadingRangeAbility03",
+    "Items.UploadReductionAbility03",
+    "Items.NPCUploadTimeAbility03"
+}
+
 local objStatsMk4 = {
     "Items.SandevistanBase_inline0",
     "Items.SandevistanBase_inline1",
@@ -124,6 +154,15 @@ local objStatsRageMk5 = {
     "Items.NetwatchQiantRageMK5_M_inline5"
 }
 
+local objStatsAcidMk6 = {
+    "Items.SandevistanBase_inline0",
+    "Items.SandevistanBase_inline1",
+    "Items.NetwatchNetdriverLegendaryMKV_inline0",
+    "Items.KangTaoAcidMK6_M_inline3",
+    "Items.KangTaoAcidMK6_M_inline4",
+    "Items.KangTaoAcidMK6_M_inline5"
+}
+
 local mk5Slots = {
     "Items.CyberdeckBlueprint8Slots_inline1",
     "Items.CyberdeckBlueprint8Slots_inline2",
@@ -133,6 +172,19 @@ local mk5Slots = {
     "Items.CyberdeckBlueprint8Slots_inline6",
     "Items.CyberdeckBlueprint8Slots_inline7",
     "Items.SandevistanBlueprint_inline1"
+}
+
+local mk6Slots = {
+    "Items.CyberdeckBlueprint8Slots_inline1",
+    "Items.CyberdeckBlueprint8Slots_inline2",
+    "Items.CyberdeckBlueprint8Slots_inline3",
+    "Items.CyberdeckBlueprint8Slots_inline4",
+    "Items.CyberdeckBlueprint8Slots_inline5",
+    "Items.CyberdeckBlueprint8Slots_inline6",
+    "Items.CyberdeckBlueprint8Slots_inline7",
+    "Items.CyberdeckBlueprint8Slots_inline8",
+    "Items.SandevistanBlueprint_inline1",
+    "Items.SandevistanBlueprint_inline2"
 }
 
 local nm4Price = {
@@ -173,12 +225,39 @@ local nm5SellPrice = {
     "Price.Cyberdeck_NM5"
 }
 
+local mk6Price = {
+    "Price.BasePrice",
+    "Price.BuyMultiplier",
+    "Price.Cyberware",
+    "Price.CyberwareQualityMultiplier",
+    "Price.IconicMultiplier",
+    "Price.Cyberdeck_Hybrid_MK6"
+}
+
+local mk6SellPrice = {
+    "Price.BasePrice",
+    "Price.CyberwareSellMultiplier",
+    "Price.Cyberware",
+    "Price.CyberwareQualityMultiplier",
+    "Price.CraftingTraitMultiplier",
+    "Price.IconicMultiplier",
+    "Price.Cyberdeck_Hybrid_MK6"
+}
+
 -- Custom 8Slots Blueprint for hybrid cyberdecks Mk.5
 if TweakDB:GetRecord("Items.NightmareCyberdeckBlueprint8Slots") == nil then
     TweakDB:CreateRecord("Items.NightmareCyberdeckBlueprint8Slots", "gamedataItemBlueprint_Record")
     TweakDB:CloneRecord("Items.NightmareCyberdeckBlueprint8Slots_inline0", "Items.CyberdeckBlueprint8Slots_inline0")
     TweakDB:SetFlat("Items.NightmareCyberdeckBlueprint8Slots.rootElement", "Items.NightmareCyberdeckBlueprint8Slots_inline0")
     TweakDB:SetFlat("Items.NightmareCyberdeckBlueprint8Slots_inline0.childElements", mk5Slots)
+end
+
+-- Custom 10Slots Blueprint for hybrid cyberdecks Mk.6
+if TweakDB:GetRecord("Items.HybridCyberdeckBlueprint10Slots") == nil then
+    TweakDB:CreateRecord("Items.HybridCyberdeckBlueprint10Slots", "gamedataItemBlueprint_Record")
+    TweakDB:CloneRecord("Items.HybridCyberdeckBlueprint10Slots_inline0", "Items.CyberdeckBlueprint8Slots_inline0")
+    TweakDB:SetFlat("Items.HybridCyberdeckBlueprint10Slots.rootElement", "Items.HybridCyberdeckBlueprint10Slots_inline0")
+    TweakDB:SetFlat("Items.HybridCyberdeckBlueprint10Slots_inline0.childElements", mk6Slots)
 end
 
 -- Arasaka Nightmare Mk.4
@@ -230,6 +309,23 @@ TweakDB:SetFlat("Items.NetwatchQiantRageMK5_M.sellPrice", nm5SellPrice)
 TweakDB:SetFlat("Items.NetwatchQiantRageMK5_M.statModifiers", objStatsRageMk5)
     TweakDB:SetFlat("Items.NetwatchQiantRageMK5_M_inline0.stats", {"Items.NetwatchQiantRageMK5_M_inline1","Items.NetwatchQiantRageMK5_M_inline2"})
     TweakDB:SetFlat("Items.NetwatchQiantRageMK5_M_inline6.valueToCheck", 12)   -- Prereq
+
+-- Kang Tao Acid Mk.6
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.blueprint", "Items.HybridCyberdeckBlueprint10Slots")
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.buyPrice", mk6Price)
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.cyberwareType", CName('Cyberdeck'))
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.displayName", LocKey("KangTaoAcidMK6-Name"))
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.equipPrereqs", {"Items.KangTaoAcidMK6_M_inline6"})
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.friendlyName", "Kang Tao Acid Mk.6")
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.iconPath", "cw_system_netwatchnetdrivercyberdeck")
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.localizedDescription", LocKey("KangTaoAcidMK6-Desc"))
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.objectActions", objActions)
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.OnEquip", objEquipHybridMk6)
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.quality", "Quality.Legendary")
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.sellPrice", mk6SellPrice)
+TweakDB:SetFlat("Items.KangTaoAcidMK6_M.statModifiers", objStatsAcidMk6)
+    TweakDB:SetFlat("Items.KangTaoAcidMK6_M_inline0.stats", {"Items.KangTaoAcidMK6_M_inline1","Items.KangTaoAcidMK6_M_inline2"})
+    TweakDB:SetFlat("Items.KangTaoAcidMK6_M_inline6.valueToCheck", 12)   -- Prereq
 
 -- NetWatch & Qiant Rage Mk.5 TEST
 if TweakDB:GetRecord("Items.NetwatchQiantRageMK5_Test") == nil then
